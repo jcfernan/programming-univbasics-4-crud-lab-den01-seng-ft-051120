3608af2535750ca1b791ea9d54bba8527ebbf318
Juan Fernandez <jcfernan@live.com>
2020-04-09 01:31:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  fruit= ["apple", "orange", "pina"]
+  fruit= ["wow", "apple", "orange", "pina"]
   fruit[0]
 end
 
