3141a6366becf751ca0ece5d4716567a6b056de7
Juan Fernandez <jcfernan@live.com>
2020-04-09 01:30:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   superhero= ["superman", "batman", "flash"]
-  superhero[1]
+  superhero[2]
 end
 
 def retrieve_first_element_from_array(array)
